b036c94200064f5cfe961787a2db37c0679cbbaa
Gustav Sörnäs <gustav@sornas.net>
2020-04-04 01:17:55 +0200

    add struct for point (and connection, unused)

--- src/track.c
+++ src/track.c
@@ -5,8 +5,8 @@
 
 void add_point(Segment *to_segment, Vec2 point) {
     to_segment->num_points++;
-    to_segment->points = realloc(to_segment->points, to_segment->num_points * sizeof(Vec2));
-    to_segment->points[to_segment->num_points - 1] = point;
+    to_segment->points = realloc(to_segment->points, to_segment->num_points * sizeof(Point));
+    to_segment->points[to_segment->num_points - 1] = (Point) { point, to_segment };
 }
 
 Segment *add_segment(Track *to_track) {
@@ -21,12 +21,12 @@ void draw_track(Track *track) {
         Segment segment = track->segments[i];
         if (segment.num_points < 2) continue;
 
-        fog_renderer_push_line(0, segment.points[0], segment.points[segment.num_points-1], fog_V4(0, 0, 0, 1), TRACK_POINT_SIZE/2);
-        fog_renderer_push_point(0, segment.points[0], TRACK_ENDPOINT_COLOR, TRACK_POINT_SIZE);
-        fog_renderer_push_point(0, segment.points[segment.num_points-1], TRACK_ENDPOINT_COLOR, TRACK_POINT_SIZE);
+        fog_renderer_push_line(0, segment.points[0].point, segment.points[segment.num_points-1].point, fog_V4(0, 0, 0, 1), TRACK_POINT_SIZE/2);
+        fog_renderer_push_point(0, segment.points[0].point, TRACK_ENDPOINT_COLOR, TRACK_POINT_SIZE);
+        fog_renderer_push_point(0, segment.points[segment.num_points-1].point, TRACK_ENDPOINT_COLOR, TRACK_POINT_SIZE);
 
         for (u32 j = 1; j < segment.num_points-1; j++) {
-            fog_renderer_push_point(0, segment.points[j], TRACK_CONTROLPOINT_COLOR, TRACK_POINT_SIZE);
+            fog_renderer_push_point(0, segment.points[j].point, TRACK_CONTROLPOINT_COLOR, TRACK_POINT_SIZE);
         }
     }
 }
--- src/track.h
+++ src/track.h
@@ -1,15 +1,32 @@
 #define TRACK_ENDPOINT_COLOR fog_V4(1, 1, 1, 0.8)
 #define TRACK_CONTROLPOINT_COLOR fog_V4(1, 1, 1, 0.5)
 #define TRACK_POINT_SIZE 0.04
-typedef struct {
-    u32 num_points;
-    Vec2 *points;
-} Segment;
 
-typedef struct {
+typedef struct Point Point;
+typedef struct Segment Segment;
+typedef struct Connection Connection;
+typedef struct Track Track;
+
+struct Point {
+    Vec2 point;
+    Segment *parent;
+};
+
+struct Segment {
+    u32 num_points;
+    Point *points;
+};
+
+struct Connection {
+    Vec2 point;
+    u32 num_other_points;
+    Vec2 *other_points;
+};
+
+struct Track {
     u32 num_segments;
     Segment *segments;
-} Track;
+};
 
 void draw_track(Track *track);
 
