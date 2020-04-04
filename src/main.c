c9e4e9465e122562e543bdb155882071540044cb
Gustav Sörnäs <gustav@sornas.net>
2020-04-04 23:56:13 +0200

    clean up, restart

--- src/main.c
+++ src/main.c
@@ -1,19 +1,11 @@
 #include <fog.h>
-#include <stdlib.h>
 
 #include "main.h"
 #include "track.h"
 
 Track track = {};
 
-Vec2 *v1, *v2;
-
 void update() {
-    fog_util_tweak_show("");
-    fog_util_tweak_show("");
-    fog_util_tweak_show("");
-    fog_util_tweak_vec2("p1", v1, 0.2);
-    fog_util_tweak_vec2("p2", v2, 0.2);
 }
 
 void draw() {
@@ -29,34 +21,6 @@ int main(int argc, char **argv) {
     fog_renderer_set_window_size(800, 800);
     fog_renderer_turn_on_camera(0);
     fog_renderer_fetch_camera(0)->zoom = 0.8;
-
-    {
-        Segment *segment;
-
-        segment = add_segment(&track);
-        add_point(segment, fog_V2( 0.5, 0));
-        add_point(segment, fog_V2(-0.5, 0));
-
-        segment = add_segment(&track);
-        add_point(segment, fog_V2(-0.5, 0));
-        add_point(segment, fog_V2(-1  , 0));
-        add_point(segment, fog_V2(0, 0.25));
-        add_point(segment, fog_V2(-1  , 0.5));
-        add_point(segment, fog_V2(-0.5, 0.5));
-
-        v1 = &segment->points[1].point;
-        v2 = &segment->points[2].point;
-
-        segment = add_segment(&track);
-        add_point(segment, fog_V2(-0.5, 0.5));
-        add_point(segment, fog_V2( 0.5, 0.5));
-
-        segment = add_segment(&track);
-        add_point(segment, fog_V2(0.5, 0.5));
-        add_point(segment, fog_V2(1  , 0.5));
-        add_point(segment, fog_V2(1  , 0));
-        add_point(segment, fog_V2(0.5, 0));
-    }
 
     fog_run(update, draw);
     return 0;
--- src/track.c
+++ src/track.c
@@ -5,35 +5,7 @@
 
 #include "track.h"
 
-Point *add_point(Segment *to_segment, Vec2 point) {
-    to_segment->num_points++;
-    to_segment->points = realloc(to_segment->points, to_segment->num_points * sizeof(Point));
-    to_segment->points[to_segment->num_points - 1] = (Point) { point, to_segment };
-    return &to_segment->points[to_segment->num_points - 1];
-}
-
-Segment *add_segment(Track *to_track) {
-    to_track->num_segments++;
-    to_track->segments = realloc(to_track->segments, to_track->num_segments * sizeof(Segment));
-    to_track->segments[to_track->num_segments - 1] = (Segment) { 0, NULL };
-    return &to_track->segments[to_track->num_segments - 1];
-}
-
-void draw_track(Track *track) {
-    for (u32 i = 0; i < track->num_segments; i++) {
-        Segment segment = track->segments[i];
-        if (segment.num_points < 2) continue;
-
-        push_bezier(segment);
-        //fog_renderer_push_line(0, segment.points[0].point, segment.points[segment.num_points-1].point, fog_V4(0, 0, 0, 1), TRACK_POINT_SIZE/2);
-        fog_renderer_push_point(0, segment.points[0].point, TRACK_ENDPOINT_COLOR, TRACK_POINT_SIZE);
-        fog_renderer_push_point(0, segment.points[segment.num_points-1].point, TRACK_ENDPOINT_COLOR, TRACK_POINT_SIZE);
-
-        for (u32 j = 1; j < segment.num_points-1; j++) {
-            fog_renderer_push_point(0, segment.points[j].point, TRACK_CONTROLPOINT_COLOR, TRACK_POINT_SIZE);
-        }
-    }
-}
+void draw_track(Track *track) {}
 
 void get_bezier(u32 num_points, Vec2 *points) {
     f32 len = 0.0f;
@@ -56,6 +28,5 @@ void get_bezier(u32 num_points, Vec2 *points) {
         len += sqrt(SQ(x, old_x) + SQ(y, old_y));
 #undef SQ
     }
-    return;
 }
 
