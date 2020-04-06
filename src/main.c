3f37ee517470095120bb44861612ad1c0e74f3da
Gustav Sörnäs <gustav@sornas.net>
2020-04-06 17:50:38 +0200

    click+drag move camera, support zoom

--- src/main.c
+++ src/main.c
@@ -9,10 +9,12 @@ Track track = {};
 
 void update() {
     Vec2 move = fog_input_mouse_move();
+    Camera *camera = fog_renderer_fetch_camera(0);
+    f32 zoom = camera->zoom;
     if (fog_input_mouse_down(0)) {
-        fog_renderer_fetch_camera(0)->position = fog_add_v2(
-                fog_renderer_fetch_camera(0)->position,
-                fog_V2(-move.x/400.f, move.y/400.0f));
+        camera->position = fog_add_v2(
+                camera->position,
+                fog_V2(-move.x/(400.f * zoom), move.y/(400.0f * zoom)));
     }
 }
 
@@ -28,7 +30,7 @@ int main(int argc, char **argv) {
     fog_init(argc, argv);
     fog_renderer_set_window_size(800, 800);
     fog_renderer_turn_on_camera(0);
-    fog_renderer_fetch_camera(0)->zoom = 1;
+    fog_renderer_fetch_camera(0)->zoom = 0.5;
 
     {
         Track *t = &track;
